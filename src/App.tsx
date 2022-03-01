7cb8b86f4708c4c419f815e1ebaf6b564927bb54
Kamil Domagalski <domagalski.kamil@gmail.com>
2022-03-01 14:54:41 +0100

    Add MovieList component and introduce Router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Switch, Route } from 'react-router-dom';
 import ViewLayout from './components/ViewLayout/ViewLayout';
+import MovieList from './components/MovieList/MovieList';
 
 import './app.scss';
 
 function App() {
   return (
     <ViewLayout>
-      <p>'Hello worldd'</p>
+      <Switch>
+        <Route exact path={'/'}>
+          <MovieList />
+        </Route>
+      </Switch>
     </ViewLayout>
   );
 }
---
+++ src/components/MovieList/MovieList.tsx
@@ -0,0 +1,3 @@
+export default function MovieList(): JSX.Element {
+  return <section>Movies List</section>;
+}
