54f23b5e37653d6000ae2ea2dbfd8fd395ef4b88
Kamil Domagalski <domagalski.kamil@gmail.com>
2022-03-02 12:55:19 +0100

    Add Card component with tets

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,31 @@
+import { useCallback } from 'react';
+import { Movie } from '../../types/types';
+import styles from './card.module.scss';
+
+interface Props extends Movie {
+  handleShowInfo: (id: number) => void;
+}
+export default function Card({ ...props }: Props): JSX.Element {
+  const { id, original_language, original_title, overview, poster_path, release_date, vote_average, handleShowInfo } =
+    props;
+
+  const handleDisplay = useCallback(() => handleShowInfo(id), [id, handleShowInfo]);
+  return (
+    <div className={styles.wrapper} onClick={handleDisplay}>
+      <div className={styles.poster}>
+        <img data-testid='image' src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt='' />
+      </div>
+      <h2 data-testid='title' className={styles.title}>
+        {original_title} ({original_language})
+      </h2>
+      <div className={styles.info}>
+        <p data-testid='releaseDate'>Date of release: {release_date}</p>
+        <p data-testid='voteAverage'>Average vote: {vote_average}</p>
+      </div>
+      <div className={styles.descrition}>
+        <p>Description</p>
+        <p data-testid='overview'>{overview}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Card/__tests__/card.test.js
@@ -0,0 +1,30 @@
+import { render, cleanup } from '../../../utils/test-utils';
+import Card from '../Card';
+
+afterEach(cleanup);
+
+const mockedProps = {
+  id: 634649,
+  original_language: 'en',
+  original_title: 'Spider-Man',
+  overview: 'Lorem ipsum',
+  poster_path: '/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg',
+  release_date: '2021-12-15',
+  vote_average: 8.3,
+  handleShowInfo: jest.fn(),
+};
+
+it('rednders component without crashing', () => {
+  render(<Card {...mockedProps} />);
+});
+
+it('renders contents correctly', () => {
+  const { getByTestId } = render(<Card {...mockedProps} />);
+  const { original_language, original_title, overview, poster_path, release_date, vote_average } = mockedProps;
+
+  expect(getByTestId('image').src).toContain(`https://image.tmdb.org/t/p/w500/${poster_path}`);
+  expect(getByTestId('title')).toHaveTextContent(`${original_title} (${original_language})`);
+  expect(getByTestId('releaseDate')).toHaveTextContent(`Date of release: ${release_date}`);
+  expect(getByTestId('voteAverage')).toHaveTextContent(`Average vote: ${vote_average}`);
+  expect(getByTestId('overview')).toHaveTextContent(overview);
+});
